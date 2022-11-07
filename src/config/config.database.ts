6bde4663b894408b658540f02f490f6ebaf38d7c
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2022-11-07 15:36:32 -0500

    Update config.database.ts

--- src/config/config.database.ts
+++ src/config/config.database.ts
@@ -1,15 +1,16 @@
 import { Sequelize } from "sequelize"
 
-const db = new Sequelize('d7qoicdc04he4k', 'postgres', '58c366e665f06a9f11c0177a88fe24ac7475cedb3d1107eb34055911926af44f', {
-    host: 'ec2-44-205-177-160.compute-1.amazonaws.com',//localhost
+const db = new Sequelize('railway', 'postgres', '28kWK6WV3DIiIjTwupLl', {
+    host: 'containers-us-west-111.railway.app',//localhost
     dialect: 'postgres',
     logging: false,
-    port:5432,
+    port:6089,
     protocol: 'postgres',
     dialectOptions: {
       ssl: process.env.DB_ENABLE_SSL && {
-        require: true
+        require: true,
+        rejectUnauthorized: false 
       }
     }
 });
-export default db;
+export default db; db;
